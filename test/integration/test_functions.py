a8cbf4911212aaf2a1b39d6c2ad919eba74ebc25
fittingIntelligence <68727587+fittingIntelligence@users.noreply.github.com>
2024-12-21 13:44:45 +1100

    Update test_functions.py

--- test/integration/test_functions.py
+++ test/integration/test_functions.py
@@ -65,7 +65,7 @@ def display_gradient(display):
     display.draw_partial(constants.DisplayModes.DU)
 
 def display_image_8bpp(display):
-    img_path = 'images/sleeping_penguin.png'
+    img_path = 'images/IMG_0698.png'
     print('Displaying "{}"...'.format(img_path))
 
     # clearing image to white
