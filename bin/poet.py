2e8fdde815ee54ca6732bfc81e67cfd1a4ab63aa
Byron Kyriakides <byron@fittingintelligence.com>
2025-01-04 10:10:01 +1100

    asdf

--- bin/poet.py
+++ bin/poet.py
@@ -42,7 +42,7 @@ ui_control.clear_display()
 ui_control.display_image_8bpp()
 ui_control.print_system_info()
 
-ui_control.partial_update_msg('...','')
+ui_control.partial_update_msg('Onwards to your writing adventures','')
 ui_control.sys_msg(ui_control.system_info,'')
 
 ls = file_operations.loadscreen()
