6689c1869a460263f4fa1566ac805ccfc4440b3f
Byron Kyriakides <byron@fittingintelligence.com>
2025-01-03 18:35:24 +1100

    asdf

--- bin/poet.py
+++ bin/poet.py
@@ -35,8 +35,8 @@ epd = ui_control.display.epd
 print('VCOM set to', epd.get_vcom())
 
 ui_control.clear_display()
-# ui_control.display_image_8bpp()
-# ui_control.print_system_info()
+ui_control.display_image_8bpp()
+ui_control.print_system_info()
 
 ui_control.partial_update_msg('...','')
 # ui_control.backspace(0, 0, 'XXXxx', 'XXXxxXXX')
