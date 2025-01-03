6440465d477f394866596107fad27133146267f9
Byron Kyriakides <byron@fittingintelligence.com>
2025-01-03 21:05:47 +1100

    clear coords working for file load selector

--- bin/poet.py
+++ bin/poet.py
@@ -111,13 +111,13 @@ try:
 
         if current_window == ['loadscreen','down'] :
             ls.move_down()
-            ui_control.paint_coords( 15*0x10, 100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110, 600)
+            ui_control.clear_coords(100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110, 600)
             ui_control.partial_update_msg( '\n' * ls.position +  '|'  ,'')
             ko.window = ['loadscreen','wait']
             
         if current_window == ['loadscreen','up']:
             ls.move_up()
-            ui_control.paint_coords( 5*0x10, 100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110, 600)
+            ui_control.clear_coords(100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110, 600)
             ui_control.partial_update_msg(  '\n' * ls.position +  '|'  ,'')
             ko.window = ['loadscreen','wait']
                 
