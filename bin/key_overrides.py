3b044662914d03b3d2290eb3fd74161a3250838e
Byron Kyriakides <byron@fittingintelligence.com>
2025-01-03 14:00:28 +1100

    added try to keypress

--- bin/key_overrides.py
+++ bin/key_overrides.py
@@ -43,87 +43,89 @@ class keyboard_overrides:
 
     def handle_key_press(self, e):
         print(f'key pressed {e}')
-        print(e.name)
-        if e.name == "backspace":
-            self.delete_character()
-            needs_input_update = True
-            input_catchup = True
+        try:
+            if e.name == "backspace":
+                self.delete_character()
+                needs_input_update = True
+                input_catchup = True
 
-        elif e.name == 'caps lock': 
-            self.capslock_active = True if self.capslock_active == False else False
-            print('caps lock')
+            elif e.name == 'caps lock': 
+                self.capslock_active = True if self.capslock_active == False else False
+                print('caps lock')
 
+                    
+            elif e.name == "space": #space bar
+                self.insert_character(" ")
                 
-        elif e.name == "space": #space bar
-            self.insert_character(" ")
-            
-            # Check if adding the character exceeds the line length limit
-            if cursor_position > self.chars_per_line:
-                self.previous_lines.append(input_content)                
-                input_content = ""
-                needs_display_update = True
-            # Update cursor_position to the length of the remaining input_content
-            cursor_position = len(input_content)
-            
-            needs_input_update = True
-            input_catchup = True
-        
-        elif e.name == "enter":
-            if scrollindex>1:
-                #if you were reviewing text, jump to scrollindex=1
-                scrollindex = 1
-                update_display()
-            else:
-                # Add the input to the previous_lines array
-                previous_lines.append(input_content)
-                input_content = "" #clears input content
-                cursor_position=0
-                #save the file when enter is pressed
-                save_previous_lines(file_path, previous_lines)
-                needs_display_update = True
+                # Check if adding the character exceeds the line length limit
+                if cursor_position > self.chars_per_line:
+                    self.previous_lines.append(input_content)                
+                    input_content = ""
+                    needs_display_update = True
+                # Update cursor_position to the length of the remaining input_content
+                cursor_position = len(input_content)
+                
+                needs_input_update = True
                 input_catchup = True
             
-        if e.name == 'ctrl': #if control is released
-            self.control_active = False 
-
-        if e.name == 'shift': #if shift is released
-            self.shift_active = False
-            
-
-        elif len(e.name) == 1 and self.control_active == False:  # letter and number input
-            
-            if self.shift_active:
-                char = keymaps.shift_mapping.get(e.name)
-                self.input_content += char
-            elif self.capslock_active:
-                char = keymaps.shift_mapping.get(e.name)
-                self.input_content += char
+            elif e.name == "enter":
+                if scrollindex>1:
+                    #if you were reviewing text, jump to scrollindex=1
+                    scrollindex = 1
+                    update_display()
+                else:
+                    # Add the input to the previous_lines array
+                    previous_lines.append(input_content)
+                    input_content = "" #clears input content
+                    cursor_position=0
+                    #save the file when enter is pressed
+                    save_previous_lines(file_path, previous_lines)
+                    needs_display_update = True
+                    input_catchup = True
                 
-            else:
-                self.input_content += e.name
+            if e.name == 'ctrl': #if control is released
+                self.control_active = False 
+
+            if e.name == 'shift': #if shift is released
+                self.shift_active = False
                 
-            self.cursor_position += 1
+
+            elif len(e.name) == 1 and self.control_active == False:  # letter and number input
+                
+                if self.shift_active:
+                    char = keymaps.shift_mapping.get(e.name)
+                    self.input_content += char
+                elif self.capslock_active:
+                    char = keymaps.shift_mapping.get(e.name)
+                    self.input_content += char
+                    
+                else:
+                    self.input_content += e.name
+                    
+                self.cursor_position += 1
+                self.needs_input_update = True
+
+                # Check if adding the character exceeds the line length limit
+                if self.cursor_position > self.chars_per_line:
+                    # Find the last space character before the line length limit
+                    last_space = self.input_content.rfind(' ', 0, self.chars_per_line)
+                    sentence = self.input_content[:last_space]
+                    # Append the sentence to the previous lines
+                    self.previous_lines.append(sentence)                
+
+                    # Update input_content to contain the remaining characters
+                    self.input_content = self.input_content[last_space + 1:]
+                    needs_display_update=True
+                    
+                # Update cursor_position to the length of the remaining input_content
+                self.cursor_position = len(self.input_content)                
+
+            self.typing_last_time = time.time()
+            self.input_catchup==True
             self.needs_input_update = True
-
-            # Check if adding the character exceeds the line length limit
-            if self.cursor_position > self.chars_per_line:
-                # Find the last space character before the line length limit
-                last_space = self.input_content.rfind(' ', 0, self.chars_per_line)
-                sentence = self.input_content[:last_space]
-                # Append the sentence to the previous lines
-                self.previous_lines.append(sentence)                
-
-                # Update input_content to contain the remaining characters
-                self.input_content = self.input_content[last_space + 1:]
-                needs_display_update=True
-                
-            # Update cursor_position to the length of the remaining input_content
-            self.cursor_position = len(self.input_content)                
-
-        self.typing_last_time = time.time()
-        self.input_catchup==True
-        self.needs_input_update = True
-        self.keypressed=True
+            self.keypressed=True
+        except (e):
+            print(e)
 
             
     def handle_interrupt(signal, frame):
