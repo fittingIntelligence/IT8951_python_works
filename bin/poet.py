7465cb7bad8fd33d87be4833c24106e79035dead
Byron Kyriakides <byron@fittingintelligence.com>
2025-01-03 21:16:10 +1100

    adsf

--- bin/poet.py
+++ bin/poet.py
@@ -106,7 +106,7 @@ try:
 
         if current_window == ['loadscreen','down'] :
             ls.move_down()
-            ui_control.clear_coords(100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110, 600)
+            ui_control.clear_coords(100, 100, 110, 600)
             ui_control.fill_coords( 100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110,100 + (ls.position * ui_control.font_height_per_line))
             
             # ui_control.partial_update_msg( '\n' * ls.position +  '|'  ,'')
@@ -114,7 +114,7 @@ try:
             
         if current_window == ['loadscreen','up']:
             ls.move_up()
-            ui_control.clear_coords(100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110, 600)
+            ui_control.clear_coords(100, 100, 110, 600)
             ui_control.fill_coords( 100, 100 + (ls.prev_position * ui_control.font_height_per_line), 110,100 + (ls.position * ui_control.font_height_per_line))
             ko.window = ['loadscreen','wait']
                 
