bfeec3e02f1790bafd6b8286743fa86652fd7487
Byron Kyriakides <byron@fittingintelligence.com>
2025-01-06 19:41:58 +1100

    asdf

--- bin/poet.py
+++ bin/poet.py
@@ -50,7 +50,6 @@ ui_control.print_system_info()
 ui_control.partial_update_msg('...','..')
 ui_control.sys_msg(ui_control.system_info,'')
 
-ui_control.write_text(100, 1340, startup_datetime, 0, 0, 1800, 1400)
 
 ls = file_operations.loadscreen()
 ko = keyboard_overrides()
@@ -150,6 +149,7 @@ try:
         elif current_window == ['write','open']:
             # ui_control.clear_display()
             ui_control.display_image_8bpp(ui_backgrounds['gs'])
+            ui_control.write_text(100, 1340, startup_datetime, 0, 0, 1800, 1400)
             ui_control.partial_update_msg('...','..')
             ko.window = ['write','ready']
                 
