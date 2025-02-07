cdc836e7a4a69153b18ed7de64190ff72e6fc39b
Byron Kyriakides <byron@fittingintelligence.com>
2025-02-08 10:07:24 +1100

    asdf

--- bin/poetree_ctrl.py
+++ bin/poetree_ctrl.py
@@ -17,7 +17,7 @@ class poetree:
         self.ui.clear_display()
         
     def key_watcher(self,e):
-        key_sequence = self.kb.handle_key_press(self, e)
+        key_sequence = self.kb.handle_key_press(e)
         self.partial_update_msg(''.join(key_sequence),self.content)
         self.content += key_sequence
     
